5cedaa60af8f8800d6fd47d0102e5a172516144e
Vinit Parakh <vparakh@usc.edu>
2014-10-10 13:22:09 -0700

    Changing directory stucture

--- python/imageurl.py
+++
@@ -1,5 +0,0 @@
-def imageurl(photo_reference):
-        if photo_reference:
-                return "https://maps.googleapis.com/maps/api/place/photo?maxwidth=1600&photoreference="+ photo_reference
-        else:
-                return ""
