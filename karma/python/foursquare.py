eb1ee39be01f8a7dd641fc045fea7d7e1cf88780
szeke <szekely1401@gmail.com>
2015-05-08 19:39:39 -0700

    Revise FS model

---
+++ karma/python/foursquare.py
@@ -0,0 +1,5 @@
+
+
+def photoGetRequest(venueId):
+	"""https://api.foursquare.com/v2/venues/43695300f964a5208c291fe3/photos?oauth_token=GGCGJ04UILGC43NGC2MRQODT1WQQQCN5EQCPORAWSIBPWKEU&v=20150508"""
+	return "https://api.foursquare.com/v2/venues/"+venueId+"/photos?oauth_token="
