eb75f05684bc33f6a06f5b0e3cf40b73102951b2
Andrew Philpot <andrew.philpot@gmail.com>
2015-06-01 15:45:35 -0700

    stub functions for OSM

--- karma/python/osm.py
+++ karma/python/osm.py
@@ -4,3 +4,8 @@ def buildingUri(buildingId):
 
 def streetUri(streetId):
     return "osm/street/%s" % streetId
+
+def kml2globalLocationNumber(kml):
+    """<Point><coordinates>41.7634935,-72.6830523</coordinates></Point>"""
+    (lon,lat) = kml[20:-22].split(',')
+    return "%s, %s" % (lat, lon)
